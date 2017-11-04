985b8d13912e9657cd3b02435359c49f6eb9df5a
DESKTOP-NDAAIBL\14069 <1406957473@qq.com>
2017-11-05 00:01:19 +0800

    项目初始构建化

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,69 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="0">
+        <mt-cell title="标题" label="描述信息" is-link></mt-cell>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="1">
+        <wait></wait>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        2.2
+      </mt-tab-container-item>
+    </mt-tab-container>
+
+
+    <mt-tabbar v-model="selected" @click.native="tabbarOnClick">
+      <mt-tab-item id="0" >
+        <i slot="icon" class="iconfont icon-men" />
+        款式
+      </mt-tab-item>
+      <mt-tab-item id="1">
+        <i slot="icon" class="iconfont icon-jisuanqi" />
+        计算
+      </mt-tab-item>
+      <mt-tab-item id="2">
+        <i slot="icon" class="iconfont icon-guanyuwomen-copy" />
+        关于我们
+      </mt-tab-item>
+    </mt-tabbar>
+
   </div>
 </template>
 
 <script>
+import wait from './views/Wait'
+
 export default {
-  name: 'app'
+  components: { wait },
+  name: 'app',
+  data () {
+    return {
+      selected: '0'
+    }
+  },
+  mounted () {
+    console.log('开始执行App')
+  },
+  methods: {
+    tabbarOnClick () {
+      console.log(this.selected)
+    }
+  }
+
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+import { Tabbar, TabItem, TabContainer, TabContainerItem, Cell } from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+import './assets/fontIcons/iconfont.css'
+
+import App from './App'
+
+Vue.config.productionTip = true
+
+Vue.component(Tabbar.name, Tabbar)
+Vue.component(TabItem.name, TabItem)
+Vue.component(TabContainer.name, TabContainer)
+Vue.component(TabContainerItem.name, TabContainerItem)
+Vue.component(Cell.name, Cell)
 
 /* eslint-disable no-new */
 new Vue({
